60d0a1b9909dc582a9cd092b6c7b8a4a4987991b
amineaouini-notbot <aminaouini565@gmail.com>
2024-02-12 23:33:15 +0100

    add to next with recursion

--- src/LinkedList.java
+++ src/LinkedList.java
@@ -9,7 +9,19 @@ public class LinkedList {
 	public static void main(String args[]) {
 		LinkedList myLinked = new LinkedList(5);
 		System.out.println(myLinked.val);
+		myLinked.add(8);
+		myLinked.add(91);
+		System.out.println(myLinked.next.val+ " and " + myLinked.next.next.val + " add into next");
 	}
-	
+	public int add(int value) {
+		
+		if(next == null) {
+			next = new LinkedList(value);
+			return value;
+		} else {
+			return next.add(value);
+		}
+		
+	}
 }
 
