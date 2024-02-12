7b2d57b7bed03e9a6433e7ca9f2f3a460423b6b9
amineaouini-notbot <aminaouini565@gmail.com>
2024-02-12 23:39:55 +0100

    implement find method in linked list

--- src/LinkedList.java
+++ src/LinkedList.java
@@ -11,7 +11,21 @@ public class LinkedList {
 		System.out.println(myLinked.val);
 		myLinked.add(8);
 		myLinked.add(91);
+		myLinked.add(44);
 		System.out.println(myLinked.next.val+ " and " + myLinked.next.next.val + " add into next");
+		int isFound = myLinked.find(15);
+		if( isFound != 0) {
+			System.out.println(15 + " is found!!");
+		} else {
+			System.out.println(15 + " isn't found!!");
+		}
+		int isFound1 = myLinked.find(91);
+		if( isFound1 != 0) {
+			System.out.println(91 + " is found!!");
+		} else {
+			System.out.println(91 + " isn't found!!");
+		}
+		
 	}
 	public int add(int value) {
 		
@@ -22,6 +36,17 @@ public class LinkedList {
 			return next.add(value);
 		}
 		
+	}
+	
+	public int find(int value) {
+		if (val == value) {
+			return value;
+		}
+		else if( next == null) {
+			return 0;
+		} else {
+			return next.find(value);
+		}
 	}
 }
 
