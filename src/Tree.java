c0aac00aa58a1b77a84b91671fea5f69a83132d7
amineaouini-notbot <aminaouini565@gmail.com>
2024-02-15 19:29:30 +0100

    finished add to tree (right greater & left is lower)

---
+++ src/Tree.java
@@ -0,0 +1,51 @@
+
+public class Tree {
+	private int val;
+	private Tree right;
+	private Tree left;
+
+	public Tree (int value) {
+		val = value;
+		right = null;
+		left = null;
+	}
+	
+	public static void main(String args[]) {
+		Tree myTree = new Tree(8);
+		myTree.add(5);
+		myTree.add(20);
+		myTree.add(3);
+		myTree.add(30);
+		myTree.add(10);
+		
+		if(myTree.right.val == 20 && myTree.left.val == 5) {
+			System.out.println("fist sub trees work fine!!");
+		}
+		if(myTree.right.left.val == 10 && myTree.right.right.val == 30) {
+			System.out.println("second sub trees work fine!!");
+		}
+	}
+	
+	public int add(int value){
+		
+		if (value > val) {
+			if( right != null) {
+				return right.add(value);
+			}else {
+				right = new Tree(value);
+				return value;
+				
+			}
+		} else {
+			if( left != null) {
+				return left.add(value);
+			}else {
+				left = new Tree(value);
+				return value;
+				
+			}
+		}
+	}
+	
+
+}
